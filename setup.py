673868fd1fb69e1d4e3d48a9c9c2df36c85249a8
Samuel Carlsson <samuel.carlsson@volumental.com>
2020-11-29 20:25:32 +0100

    description

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="ar",
-    version="0.3.0",
+    version="0.3.1",
+    description="Access ar archive files (.a)",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     license="apache-2.0",
