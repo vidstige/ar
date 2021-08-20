8fdea3e0164239c39106b6ba1067f4f43347801c
Samuel Carlsson <samuel.carlsson@volumental.com>
2021-08-20 09:37:09 +0200

    add home page and author

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-import os
 from setuptools import setup, find_packages
 
 setup(
     name="ar",
-    version="0.3.1",
+    version="0.3.2",
     description="Access ar archive files (.a)",
+    url="http://github.com/vidstige/ar/",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     license="apache-2.0",
     packages=find_packages(),
+    author="Samuel Carlsson",
+    author_email="samuel.carlsson@gmail.com",
 )
